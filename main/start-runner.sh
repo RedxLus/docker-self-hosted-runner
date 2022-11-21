ad0545782e17a20f0446ed8f1d881b31e535854b
Luis Dieguez <luis.dieguez@promocionesfarma.com>
2022-11-21 12:46:42 +0100

    update script to eph

--- main/start-runner.sh
+++ main/start-runner.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-#REPO=""
-#PERSONAL_ACCESS_TOKEN="" <- ENV of Docker
+#REPO=""                    <- ENV of Docker
+#PERSONAL_ACCESS_TOKEN=""   <- ENV of Docker
+#RUNNER_ALLOW_RUNASROOT=""  <- ENV of Docker
+#EPHEMERAL="true"           <- ENV of Docker
+
+
+# Set "true" for ephemeral runners. Set "false" for consistent runners
 
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${PERSONAL_ACCESS_TOKEN}" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
-./config.sh --disableupdate --unattended --url https://github.com/${REPO} --token ${REG_TOKEN}
+if [[ "$EPHEMERAL" == "false" ]];
+then
+    ./config.sh --disableupdate --unattended --url https://github.com/${REPO} --token ${REG_TOKEN}
 
-cleanup() {
-    echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
-}
+    cleanup() {
+        echo "Removing runner..."
+        ./config.sh remove --unattended --token ${REG_TOKEN}
+    }
 
-trap 'cleanup; exit 130' INT
-trap 'cleanup; exit 143' TERM
+    trap 'cleanup; exit 130' INT
+    trap 'cleanup; exit 143' TERM
 
-./run.sh & wait $!
+    ./run.sh & wait $!
+else
+    ./config.sh --disableupdate --unattended --url https://github.com/${REPO} --token ${REG_TOKEN} --ephemeral
+fi
